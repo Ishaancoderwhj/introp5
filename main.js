62800b583f604cab640e89300622796186273f46
Ishaancoderwhj <120032643+Ishaancoderwhj@users.noreply.github.com>
2023-08-20 11:42:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function preload(){
+}
+
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(150,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_colour="";
+}
+
+function draw(){
+    image(video,0,0,640,480);
+    tint(tint_colour);
+}
+
+function filter_tint(){
+    tint_colour=document.getElementById("colour_name").value;
+
+}
+
+function take_snapshot(){
+    save("random.png");
+}
